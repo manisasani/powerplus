1caba8211f5c2e044914202be69eff46067e564b
mani <manisasani2005@gmail.com>
2025-02-09 21:36:14 +0330

    add tests for dietplan app and edit tests accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from .models import CustomUser, Profile
-from .forms import CustomSignupForm, ProfileForm
+from .forms import CustomSignupForm
 
 class CustomUserTests(TestCase):
     def setUp(self):
@@ -25,40 +25,6 @@ class CustomUserTests(TestCase):
 
     def test_user_str_method(self):
         self.assertEqual(str(self.user), self.user_data['email'])
-
-class ProfileTests(TestCase):
-    def setUp(self):
-        self.user_data = {
-            "email": "test@gmail.com",
-            "username": "test@gmail.com",
-            "password": "testpass123",
-            "first_name": "Test",
-            "age": 25,
-            "gender": "M",
-            "height": 175,
-            "weight": 70,
-        }
-        self.user = CustomUser.objects.create_user(**self.user_data)
-
-        self.profile_data = {
-            'user': self.user,
-            'activity_level': 'active',
-            'experience_level': 'beginner',
-            'diet_goal': 'lose weight',
-            'plan_goal': 'fitness',
-        }
-        self.profile = Profile.objects.create(**self.profile_data)
-
-    def test_profile_creations(self):
-        self.assertEqual(self.profile.user, self.user)
-        self.assertEqual(self.profile.activity_level, self.profile_data['activity_level'])
-        self.assertEqual(self.profile.experience_level, self.profile_data['experience_level'])
-        self.assertEqual(self.profile.diet_goal, self.profile_data['diet_goal'])
-        self.assertEqual(self.profile.plan_goal, self.profile_data['plan_goal'])
-
-    def test_profile_str_method(self):
-        expected_str = f"Profile of {self.user.username}"
-        self.assertEqual(str(self.profile), expected_str)
 
 class SignupViewTests(TestCase):
     def setUp(self):
@@ -87,25 +53,6 @@ class SignupViewTests(TestCase):
         invalid_data= self.signup_data.copy()
         invalid_data['age'] = 300
         form = CustomSignupForm(data=invalid_data)
-        self.assertFalse(form.is_valid())
-
-class ProfileFormTest(TestCase):
-    def setUp(self):
-        self.profile_form_data = {
-            "activity_level": "active",
-            "experience_level": "beginner",
-            "diet_goal": "lose weight",
-            "plan_goal": "fitness",
-        }
-
-    def test_profile_form_valid_data(self):
-        form =  ProfileForm(data=self.profile_form_data)
-        self.assertTrue(form.is_valid())
-
-    def test_profile_form_invalid_data(self):
-        invalid_data = self.profile_form_data.copy()
-        invalid_data['activity_level'] = 'invalid_choice'
-        form = ProfileForm(data=invalid_data)
         self.assertFalse(form.is_valid())
 
 class LoginViewTests(TestCase):
--- dietplan/tests.py
+++ dietplan/tests.py
@@ -1,3 +1,249 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from .models import DietPlan, UserSelectedDietPlan
+from accounts.models import DietPlanInfo
+from django.core.exceptions import ValidationError
+from .forms import DietGoalForm  , ActivityLevelForm 
 
-# Create your tests here.
+User = get_user_model()
+class DietPlanModelTests(TestCase):
+    def setUp(self):
+        self.valid_plan = {
+            'goal': 'lose weight',
+            'calorie_range_min': 1500,
+            'calorie_range_max':2000,
+            'protein_percentage':30,
+            'carbs_percentage':50,
+            'fat_percentage':20,
+            'breakfast': 'Test breakfast',
+            'lunch': 'Test lunch',
+            'dinner': 'Test dinnder',
+        }
+    
+    def test_create_valid_diet_plan(self):
+        plan = DietPlan.objects.create(**self.valid_plan)
+        self.assertEqual(plan.goal, 'lose weight')
+        self.assertEqual(plan.calorie_range_min, 1500)
+
+    def test_invalid_macros_total(self):
+        invalid_plan = self.valid_plan.copy()
+        invalid_plan['protein_percentage'] = 40
+        with self.assertRaises(ValidationError):
+            DietPlan.objects.create(**invalid_plan)
+
+    def test_invalid_fat_percentage(self):
+        invalid_plan = self.valid_plan.copy()
+        invalid_plan['fat_percentage'] = 15
+        with self.assertRaises(ValidationError):
+            DietPlan.objects.create(**invalid_plan)
+
+    def test_str_respresentations(self):
+        plan = DietPlan.objects.create(**self.valid_plan)
+        expected = f"Diet plan for lose weight (1500-2000 cal)"
+        self.assertEqual(str(plan), expected)
+
+class UserSelectedDietPlanTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            email = 'Test@gmail.com',
+            password = 'Test1234@',
+            gender = 'M',
+            age = 25,
+            height = 180,
+            weight = 80
+
+        )
+        self.diet_plan = DietPlan.objects.create(
+            goal = 'lose weight',
+            calorie_range_min = 1500,
+            calorie_range_max = 2000,
+            protein_percentage= 30,
+            carbs_percentage= 50,
+            fat_percentage=20,
+        )
+    def test_create_user_diet_plan(self):
+        plan = UserSelectedDietPlan.objects.create(
+            user = self.user,
+            selected_plan = self.diet_plan,
+            daily_calories = 1000
+        )
+        self.assertEqual(plan.user, self.user)
+        self.assertEqual(plan.daily_calories, 1000)
+
+    def test_unique_user_constraint(self):
+        UserSelectedDietPlan.objects.create(
+            user=self.user,
+            selected_plan = self.diet_plan,
+            daily_calories= 1000
+        )
+        with self.assertRaises(Exception):
+            UserSelectedDietPlan.objects.create(
+                user=self.user,
+                selected_plan = self.diet_plan,
+                daily_calories = 2000
+            )
+
+class DietPlanViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser4',
+            email='test@gmail.com',
+            password='testpass123',
+            gender='M',
+            age=25,
+            height=180,
+            weight=80,
+        )
+        self.diet_info = DietPlanInfo.objects.get(user=self.user)
+        self.diet_info.activity_level = 'moderately active'
+        self.diet_info.diet_goal = 'lose weight'
+        self.diet_info.save()
+        
+        self.diet_plan = DietPlan.objects.create(
+            goal= 'lose weight',
+            calorie_range_min = 1500,
+            calorie_range_max = 2500,
+            protein_percentage= 30,
+            carbs_percentage = 50,
+            fat_percentage = 20,
+        )
+
+    def test_activity_level_view_unauthorized(self):
+        response = self.client.get(reverse('dietplan:activity'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_activity_level_view_authorized(self):
+        self.client.login(username='testuser4', password='testpass123')
+        response = self.client.get(reverse('dietplan:activity'))
+        self.assertEqual(response.status_code , 200)
+
+    def test_diet_plan_view_calculations(self):
+        self.client.login(username='testuser4', password='testpass123')
+        response = self.client.get(reverse('dietplan:plan'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('daily_calories', response.context)
+        self.assertIn('macros', response.context)
+
+    def test_update_diet_plan(self):
+        self.client.login(username='testuser4', password='testpass123')
+        UserSelectedDietPlan.objects.create(
+            user=self.user,
+            selected_plan=self.diet_plan,
+            daily_calories=2000
+        )
+        response = self.client.post(reverse('dietplan:update_plan'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(UserSelectedDietPlan.objects.filter(user=self.user).count(), 0)
+
+    def test_invalid_calorie_range(self):
+        self.diet_plan.calorie_range_max = 1000
+        with self.assertRaises(ValidationError):
+            self.diet_plan.full_clean()  
+            self.diet_plan.save()
+
+class DietPlanFormTest(TestCase):
+    
+    def setUp(self):
+        self.user= User.objects.create_user(
+            username='testuser5',
+            email='testtuser@gmail.com',
+            password='passtest333',
+            gender = 'M',
+            age= 23,
+            height=180,
+            weight=80,
+
+        )
+        self.diet_info = DietPlanInfo.objects.get(user=self.user)
+        
+    def test_diet_goal_form_valid(self):
+        form_data = {'diet_goal': 'lose weight'}
+        form = DietGoalForm(
+            data=form_data,
+            instance = self.diet_info
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_acticity_level_form_valid(self):
+        form_data  ={'activity_level': 'moderately active'}
+        form = ActivityLevelForm(
+            data = form_data,
+            instance = self.diet_info
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_forms_required_fields(self):
+        form1 = DietGoalForm(data={})
+        form2 = ActivityLevelForm(data={})
+        self.assertFalse(form1.is_valid())
+        self.assertFalse(form2.is_valid())
+
+class DietPlanViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        
+        self.user = User.objects.create_user(
+            username='testuser4',
+            email='test@gmail.com',
+            password='testpass123',
+            gender='M',
+            age=25,
+            height=180,
+            weight=80,
+        )
+        self.diet_plan = DietPlan.objects.create(
+            goal= 'lose weight',
+            calorie_range_min = 1500,
+            calorie_range_max = 2500,
+            protein_percentage= 30,
+            carbs_percentage = 50,
+            fat_percentage = 20,
+        )
+
+        self.diet_info = DietPlanInfo.objects.get(user=self.user)
+        self.diet_info.activity_level = 'moderately active'
+        self.diet_info.diet_goal = 'lose weight'
+        self.diet_info.save()
+
+    def test_bmr_caclulation(self):
+        self.client.login(
+        username = 'testuser4',
+        password = 'testpass123'
+        )
+        response = self.client.get(reverse('dietplan:plan'))
+        self.assertIn('daily_calories', response.context)
+
+    def test_on_suitable_plan(self):
+        self.client.login(
+            username= 'testuser4',
+            password= 'testpass123'
+        )
+        self.diet_info.diet_goal = 'gainweight'
+        self.diet_info.save()
+        response = self.client.get(reverse('dietplan:plan'))
+        self.assertIn('error', response.context)
+
+    def test_download_pdf_unauthorized(self):
+        self.client.login(
+            username = 'testuser4',
+            password = 'testpass123'
+        )
+        response = self.client.get(reverse('dietplan:download_pdf'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_download_pdf_with_plan(self):
+        self.client.login(
+            username = 'testuser4',
+            password = 'testpass123'
+        )
+        UserSelectedDietPlan.objects.create(
+            user = self.user,
+            selected_plan = self.diet_plan,
+            daily_calories = 2000
+        )
+        response = self.client.get(reverse('dietplan:download_pdf'))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response['Content-Type'], 'application/pdf')
