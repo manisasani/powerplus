72cd726060142c46081d5fe9dbe178e4d2115aef
mani <manisasani2005@gmail.com>
2025-02-14 21:38:28 +0330

    add urls for workoutplan

--- workoutplan/urls.py
+++ workoutplan/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
+from .views import WorkoutGoalView, ExperienceLevelView, WorkoutPlanView, UpdateWorkoutPlanView , DownloadWorkoutPlanPDF
 
 app_name = 'workoutplan'
 
 urlpatterns = [
-    
+    path('goal/',WorkoutGoalView.as_view(), name='goal'),
+    path('experience/',ExperienceLevelView.as_view(), name='experience'),
+    path('plan/',WorkoutPlanView.as_view(), name='workoutplan'),
+    path('update/',UpdateWorkoutPlanView.as_view(), name='update'),
+    path('download-pdf/',DownloadWorkoutPlanPDF.as_view(), name='download_pdf')
+
 ]
