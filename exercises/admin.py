bf07c8a30ac6b7fc32b0e7f68d4f3dbaab5c792a
mani <manisasani2005@gmail.com>
2025-02-24 01:37:44 +0330

    customze admin panel for exercises app

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Muscle, Exercise
 
-# Register your models here.
+@admin.register(Muscle)
+class MuscleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'create_at']
+    search_fields = ['name', 'description']
+    list_filter = ['create_at']
+    readonly_fields = ['create_at']
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'muscle', 'tags', 'create_at']
+    search_fields = ['name', 'description', 'tags']
+    list_filter = ['muscle', 'create_at']
+    readonly_fields = ['create_at']
+    autocomplete_fields = ['muscle'] 
