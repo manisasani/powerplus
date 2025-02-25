9d11e70de31730528576d3f9c8ce287f29cd5d7d
mani <manisasani2005@gmail.com>
2025-02-25 20:47:48 +0330

    add new views for exercises

--- exercises/views.py
+++ exercises/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from django.db import models  # اضافه شد
 from .models import Muscle, Exercise
 from .serializers import MuscleSerializer, ExerciseSerializer
+from django.views.generic import ListView, DetailView
 
 class MuscleViewSet(viewsets.ModelViewSet):
     queryset = Muscle.objects.all()
@@ -30,3 +31,20 @@ class ExerciseViewSet(viewsets.ModelViewSet):
                 models.Q(tags__icontains=search)
             )
         return queryset
+    
+class MuscleListView(ListView):
+    model = Muscle
+    template_name = 'exercises/muscles_list.html'
+    context_object_name = 'muscles'
+
+class MuscleDetailView(DetailView):
+    model = Muscle
+    template_name = 'exercises/muscle_detail.html'
+    context_object_name = 'muscle'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(self, **kwargs)
+        context = super().get_context_data(**kwargs)
+        context['exercises'] = Exercise.objects.filter(muscle=self.object)
+        return context
+        
