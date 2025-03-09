623c6e21b19a318f1ddb7f92418b60279efb2ed0
mani <manisasani2005@gmail.com>
2025-03-09 20:14:32 +0330

    add admin.py for calorie app

--- calorie/admin.py
+++ calorie/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import FoodCategory, FoodItem, UserFoodLog
 
-# Register your models here.
+@admin.register(FoodCategory)
+class FoodCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+
+@admin.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'calories', 'protein', 'carbs', 'fat'] 
+    list_filter = ['category']
+    search_fields = ['name']
+    list_per_page = 20
+
+@admin.register(UserFoodLog)  
+class UserFoodLogAdmin(admin.ModelAdmin):  
+    list_display = ['user', 'food_item', 'amount', 'meal_type', 'date']
+    list_filter = ['date', 'meal_type', 'user']
+    search_fields = ['user__email', 'food_item__name']
